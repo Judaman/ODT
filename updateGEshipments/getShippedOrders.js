3ff1d9060df424e84244924d1acc2d63e47cd415
judaman <judagru@gmail.com>
2019-09-04 11:45:07 -0400

    commit

--- updateGEshipments/getShippedOrders.js
+++ updateGEshipments/getShippedOrders.js
@@ -1,21 +1,19 @@
 module.exports = {
 
   getShippedOrders:
-  //getBCproducts();
-  async function getShippedOrders() {
-    getBCbrands:async function getShippedOrders() {
+    async function getShippedOrders() {
 
-    const config = {
-      'method': 'get',
-      'url': 'https://api.bigcommerce.com/stores/jhrbcxjfkv/v2/orders',
-      'accept': 'application/json',
-      'content-type': 'application/json',
-      'headers': headers
-    }
-    let res = await axios(config)
+      const config = {
+        'method': 'get',
+        'url': 'https://api.bigcommerce.com/stores/jhrbcxjfkv/v2/orders',
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'headers': headers
+      }
+      let res = await axios(config)
 
-    //console.log(res.data.data);
-    return shippedOrders;
+      //console.log(res.data.data);
+      return shippedOrders;
     }
-    }
-  }
+}
+}
